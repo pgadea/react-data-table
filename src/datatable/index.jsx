5493c8421e2380bdc7adeea261c82968d9a7babb
Pierre <pgadea@users.noreply.github.com>
2020-09-20 12:08:36 -0500

    finished data table

---
+++ src/datatable/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Datatable({data}) {
+    const columns = data[0] && Object.keys(data[0]);
+    return (
+        <table cellPadding={0} cellSpacing={0}>
+            <thead>
+                <tr>{data[0] && columns.map((heading) => <th key={heading}>{heading}</th>)}</tr>
+            </thead>
+            <tbody>
+                {data.map((row, index) => (
+                    <tr key={index}>
+                        {columns.map((column, index) => (
+                            <td key={index}>{row[column]}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
